1c36d781025d78dd8df402f9495c41f40d8a1edf
Júlio Pacheco Stein <juliopstein@gmail.com>
2025-11-10 11:14:24 -0300

    grafos

---
+++ Proj4 - Info/Vertice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apGrafoDaSilva
+{
+    class Vertice
+    {
+        public bool foiVisitado;
+        public string rotulo;
+        private bool estaAtivo;
+        public Vertice(string rotulo)
+        {
+            this.rotulo = rotulo;
+            this.foiVisitado = false;
+            this.estaAtivo = true;
+        }
+    }
+}
